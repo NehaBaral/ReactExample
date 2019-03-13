b9b9f1180cc35d82eef98b919f2c7feb3b04b555
NehaBaral <nehabaral011@gmail.com>
2019-03-13 09:58:22 +0545

    Addform and TodoApp updated

---
+++ react-todo-1/src/component/AddForm.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+export class AddForm extends Component{
+    state = {
+        content: ''
+    };
+    handleChange = (e) => {
+        this.setState({ content: e.target.value});
+    };
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
+        this.props.addTodo({
+            ...this.state,//spreading object with spread operator
+            id: Math.random()
+        });
+        this.setState({content: ''});
+    };
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add todo</label>
+                    <input type='text' onChange={this.handleChange}/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddForm;
+    
--- react-todo-1/src/component/TodoApp.js
+++ react-todo-1/src/component/TodoApp.js
@@ -3,6 +3,7 @@ import './TodoApp.css';
 import 'materialize-css/dist/css/materialize.min.css'
 import Navbar from './Navbar';
 import TodoList from './TodoList';
+import AddForm from './AddForm';
 
 class TodoApp extends Component {
   state = {
@@ -11,12 +12,18 @@ class TodoApp extends Component {
       {id: 2, content: 'Watch movie'},
     ]
   }
+
+  addTodo = (todo) =>{
+    const todos = [...this.state.todos,todo];//spread operater
+    this.setState({todos});
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className='container'>
           <TodoList todos={this.state.todos}/>
+          <AddForm addTodo={this.addTodo}/>
         </div>
       </div>
     );
