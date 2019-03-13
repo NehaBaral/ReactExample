3fd9188f22569b1a33686a84f22322e17a389e6a
NehaBaral <nehabaral011@gmail.com>
2019-03-13 10:17:13 +0545

    updated Empty view

--- react-todo-1/src/component/TodoList.js
+++ react-todo-1/src/component/TodoList.js
@@ -3,10 +3,12 @@ import React from 'react'
 const TodoList = ({todos, deleteTodo}) => {
     return(
         <div className="collection">
-        {todos.map(todo => <p key=
+        {todos.length > 0 ? (todos.map(todo => <p key=
         {todo.id} onClick={(e)=>deleteTodo(todo.id)} 
         className="collection-item">
-        {todo.content}</p>)}
+        {todo.content}</p>)):(
+            <p>All of your task is completed</p>
+        )}
         </div>
     )
 }
